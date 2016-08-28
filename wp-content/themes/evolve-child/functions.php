3a03a5faab23a7d3e0c18177f12d3a89119873b2
Dani Lewandowski <dani@splashthat.com>
2016-08-28 18:20:59 -0400

    Fixed adding styles to put parent theme styles before child. Added styling for menu to overwrite the white/light. Starting mobile styling

--- wp-content/themes/evolve-child/functions.php
+++ wp-content/themes/evolve-child/functions.php
@@ -2,7 +2,13 @@
 /**
 * Enqueues child theme stylesheet, loading first the parent theme stylesheet.
 */
+
 function custom_enqueue_child_theme_styles() {
-    wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+    wp_register_style('parent-theme', get_template_directory_uri() . '/style.css');
+    wp_register_style('child-theme', get_stylesheet_uri(), array('parent-theme') );
+    wp_enqueue_style('child-theme');
 }
+
 add_action( 'wp_enqueue_scripts', 'custom_enqueue_child_theme_styles', 11 );
+
+?>
